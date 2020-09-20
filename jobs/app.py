a7c44f65227d38d7e7708b3dcafb339520646dc1
yukta04 <70910470+yukta04@users.noreply.github.com>
2020-09-20 15:54:46 +0530

    Base Template and Styling

--- jobs/app.py
+++ jobs/app.py
@@ -22,7 +22,7 @@ def execute_sql(sql, values=(), commit= False, single=False):
     cursor.close()
     return results
 @app.teardown_appcontext
-def close_connection():
+def close_connection(exception):
     connection=getattr(g, '_connection', None)
     if connection is not None:
         connection.close()
