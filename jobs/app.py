2a05641c3b6e6360f6d7a88d3c9ec6ba91d2cdad
yukta04 <70910470+yukta04@users.noreply.github.com>
2020-09-20 19:15:25 +0530

    Employer Reviews

--- jobs/app.py
+++ jobs/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,g,request,redirect,url_for
+from flask import Flask, render_template , g , request , redirect , url_for
 import sqlite3
 import datetime
 
@@ -47,7 +47,7 @@ def employer(employer_id):
     reviews= execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
 
-@app.route('/employer/<employer_id>/review', methods=('GET','POST')
+@app.route('/employer/<employer_id>/review', methods=('GET','POST'))
 def review(employer_id):
     if request.method == 'POST':
         review= request.form['review']
